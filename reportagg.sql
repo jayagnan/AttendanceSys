fae6d18f6f3bfec95e6bbb24fd2becfe5586140d
Jayakumar <jaya.gnan@gmail.com>
2015-02-24 23:13:56 +0530

    Report changes for attendance daywise report

--- reportagg.sql
+++ reportagg.sql
@@ -1,2 +1,4 @@
-﻿SELECT count(employeeid), date, extract(day from date), attendance
-  FROM attendance where extract(month from date) = 1 and departmentid = 'VicSSS' group by date, attendance order by date;
+﻿SELECT count(employeeid), date, extract(day from date) as day, attendance
+  FROM attendance where extract(month from date) = 2 and departmentid = 'VicSSS' group by date, attendance order by date;
+
+  SELECT count(employeeid), date + 1 as date,extract(day from date) as day, attendance FROM attendance where extract(month from date) = 1 group by date, attendance order by date
